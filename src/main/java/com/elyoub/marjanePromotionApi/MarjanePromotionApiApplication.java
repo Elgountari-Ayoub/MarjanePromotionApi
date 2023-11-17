6a5f1788000df42ef635ba9137e5dd7f83bc4ad7
Elgountari-Ayoub <elgountariayoub22@gmail.com>
2023-11-17 15:05:32 +0100

    create the spring boot project & connect to db

---
+++ src/main/java/com/elyoub/marjanePromotionApi/MarjanePromotionApiApplication.java
@@ -0,0 +1,13 @@
+package com.elyoub.marjanePromotionApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarjanePromotionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarjanePromotionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elyoub/marjanePromotionApi/MarjanePromotionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elyoub.marjanePromotionApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarjanePromotionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
